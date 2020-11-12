dbc69e0cdfff7812c97304bc2660fa1962ed618c
Art <d.art.shishkin@gmail.com>
2020-11-12 20:13:29 +0200

    27. Identity in reduce() Operator

---
+++ src/main/java/net/shyshkin/multithreadingtutorial/parallelstream/ReduceExample.java
@@ -0,0 +1,23 @@
+package net.shyshkin.multithreadingtutorial.parallelstream;
+
+import java.util.List;
+
+public class ReduceExample {
+
+    public int reduce_sum_ParallelStream(List<Integer> inputList) {
+
+        int sum = inputList
+                .parallelStream()
+                //.reduce(1, (x,y)->x+y);
+                .reduce(0, (x, y) -> x + y);
+        return sum;
+    }
+
+    public int reduce_multiply_parallelStream(List<Integer> inputList) {
+        int multiply = inputList
+                .parallelStream()
+                .reduce(1, (x, y) -> x * y);
+        return multiply;
+    }
+
+}
---
+++ src/test/java/net/shyshkin/multithreadingtutorial/parallelstream/ReduceExampleTest.java
@@ -0,0 +1,66 @@
+package net.shyshkin.multithreadingtutorial.parallelstream;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ReduceExampleTest {
+
+    ReduceExample reduceExample = new ReduceExample();
+
+    @Test
+    void reduce_ParallelStream() {
+
+        //given
+        List<Integer> inputList = List.of(1, 2, 3, 4, 5, 6, 7, 8);
+
+        //when
+        int result = reduceExample.reduce_sum_ParallelStream(inputList);
+
+        //then
+        assertEquals(36, result);
+    }
+
+    @Test
+    void reduce_ParallelStream_emptyList() {
+
+        //given
+        List<Integer> inputList = new ArrayList<>();
+
+        //when
+        int result = reduceExample.reduce_sum_ParallelStream(inputList);
+
+        //then
+        assertEquals(0, result);
+    }
+
+    @Test
+    void reduce_multiply() {
+
+        //given
+        List<Integer> inputList = List.of(1, 2, 3, 4);
+
+        //when
+        int result = reduceExample.reduce_multiply_parallelStream(inputList);
+
+        //then
+        assertEquals(24, result);
+    }
+
+    @Test
+    void reduce_multiply_emptyList() {
+
+        //given
+        List<Integer> inputList = new ArrayList<>();
+
+        //when
+        int result = reduceExample.reduce_multiply_parallelStream(inputList);
+
+        //then
+        assertEquals(1, result);
+    }
+
+}
