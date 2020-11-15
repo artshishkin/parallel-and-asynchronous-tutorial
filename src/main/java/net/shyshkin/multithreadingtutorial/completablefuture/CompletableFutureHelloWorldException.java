2eb0035337e3cebaeb18f8026c14e33f0483ef42
Art <d.art.shishkin@gmail.com>
2020-11-15 22:18:47 +0200

    50. Handling Exceptions using "whenComplete" function

--- src/main/java/net/shyshkin/multithreadingtutorial/completablefuture/CompletableFutureHelloWorldException.java
+++ src/main/java/net/shyshkin/multithreadingtutorial/completablefuture/CompletableFutureHelloWorldException.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import net.shyshkin.multithreadingtutorial.service.HelloWorldService;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 import static net.shyshkin.multithreadingtutorial.util.CommonUtil.delay;
 import static net.shyshkin.multithreadingtutorial.util.LoggerUtil.log;
@@ -68,4 +70,34 @@ public class CompletableFutureHelloWorldException {
                 .thenCombine(hiCompletableFuture, String::concat)
                 .join();
     }
+
+    public String helloWorld_3AsyncCall_whenComplete() {
+        CompletableFuture<String> hello = CompletableFuture.supplyAsync(helloWorldService::hello);
+        CompletableFuture<String> world = CompletableFuture.supplyAsync(helloWorldService::world);
+        CompletableFuture<String> hiCompletableFuture = CompletableFuture
+                .supplyAsync(() -> {
+                    delay(900);
+                    return " Hi! Completable Future!";
+                });
+        BiConsumer<String, Throwable> whenCompleteConsumer = (res, ex) -> {
+            log("Result is: " + res);
+            if (ex != null) {
+                log("Exception is: " + ex.getMessage());
+            }
+        };
+        Function<Throwable, String> exceptionHandlerFunction = ex -> {
+            log("Exception caught in EXCEPTIONALLY block is: " + ex.getMessage());
+            return "<recover hello>";
+        };
+        return hello
+                //`whenComplete` allows show exception
+                .whenComplete(whenCompleteConsumer)
+                .exceptionally(exceptionHandlerFunction)    //combine two exception methods
+                .thenCombine(world, String::concat)
+                .whenComplete(whenCompleteConsumer)
+                .thenApply(String::toUpperCase)
+                .thenCombine(hiCompletableFuture, String::concat)
+                .join();
+    }
+
 }
--- src/test/java/net/shyshkin/multithreadingtutorial/completablefuture/CompletableFutureHelloWorldExceptionTest.java
+++ src/test/java/net/shyshkin/multithreadingtutorial/completablefuture/CompletableFutureHelloWorldExceptionTest.java
@@ -2,14 +2,20 @@ package net.shyshkin.multithreadingtutorial.completablefuture;
 
 import net.shyshkin.multithreadingtutorial.service.HelloWorldService;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.function.Executable;
+import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.function.Supplier;
+
 import static net.shyshkin.multithreadingtutorial.util.CommonUtil.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -122,6 +128,77 @@ class CompletableFutureHelloWorldExceptionTest {
         assertEquals("<RECOVER WORLD> Hi! Completable Future!", result);
         //We lost value from `hello`
         timeTaken();
+    }
+
+    @Nested
+    class WhenComplete {
+
+        Supplier<BDDMockito.BDDMyOngoingStubbing<String>> hello_exception = () ->
+                given(helloWorldService.hello()).willThrow(new RuntimeException("Hello Exception"));
+        Supplier<BDDMockito.BDDMyOngoingStubbing<String>> world_exception = () ->
+                given(helloWorldService.world()).willThrow(new RuntimeException("World Exception"));
+
+        @Test
+        void helloWorld_3AsyncCall_whenComplete_happyPath() {
+            //given
+            startTimer();
+
+            //when
+            String result = cfhwe.helloWorld_3AsyncCall_whenComplete();
+
+            //then
+            assertEquals("HELLO WORLD! Hi! Completable Future!", result);
+            timeTaken();
+        }
+
+        @Test
+        void helloWorld_3AsyncCall_whenComplete_helloEx() {
+            //given
+            startTimer();
+            hello_exception.get();
+
+            //when
+            String result = cfhwe.helloWorld_3AsyncCall_whenComplete();
+
+            //then
+            assertEquals("<RECOVER HELLO> WORLD! Hi! Completable Future!", result);
+            timeTaken();
+        }
+
+        @Test
+        void helloWorld_3AsyncCall_whenComplete_worldEx() {
+            //given
+            world_exception.get();
+            startTimer();
+
+            //when
+            Executable executable = () -> {
+                String result = cfhwe.helloWorld_3AsyncCall_whenComplete();
+            };
+
+            //then
+            assertThrows(RuntimeException.class, executable);
+
+            timeTaken();
+        }
+
+        @Test
+        void helloWorld_3AsyncCall_whenComplete_doubleEx() {
+            //given
+            hello_exception.get();
+            world_exception.get();
+            startTimer();
+
+            //when
+            Executable executable = () -> {
+                String result = cfhwe.helloWorld_3AsyncCall_whenComplete();
+            };
+
+            //then
+            assertThrows(RuntimeException.class, executable);
+
+            timeTaken();
+        }
     }
 
     @AfterEach
